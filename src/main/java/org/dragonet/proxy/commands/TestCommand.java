82fc0e4653a7742c068a9341dc54e8e20b2311fc
DefinitlyEvil <theevildays@gmail.com>
2016-02-23 12:46:31 +0800

    a new test command for distance teleporting

--- src/main/java/org/dragonet/proxy/commands/TestCommand.java
+++ src/main/java/org/dragonet/proxy/commands/TestCommand.java
@@ -12,8 +12,10 @@
  */
 package org.dragonet.proxy.commands;
 
-import org.dragonet.net.packet.minecraft.ChangeDimensionPacket;
-import org.dragonet.net.packet.minecraft.LoginStatusPacket;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import org.dragonet.net.packet.minecraft.FullChunkPacket;
+import org.dragonet.net.packet.minecraft.MovePlayerPacket;
 import org.dragonet.proxy.DragonProxy;
 import org.dragonet.proxy.network.UpstreamSession;
 
@@ -22,10 +24,49 @@ public class TestCommand implements ConsoleCommand {
     @Override
     public void execute(DragonProxy proxy, String[] args) {
         UpstreamSession cli = proxy.getSessionRegister().getAll().values().toArray(new UpstreamSession[0])[0];
-        cli.sendPacket(new ChangeDimensionPacket(0f, 64f, 0f, (byte)1));
-        LoginStatusPacket stat = new LoginStatusPacket();
-        stat.status = LoginStatusPacket.PLAYER_SPAWN;
-        cli.sendPacket(stat);
+
+        cli.sendChat("Initiating... ");
+
+        if (args.length == 1 || args[0] == "chunk") {
+            cli.sendChat("Sending far chunk... ");
+            try {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                byte[] yPreset = new byte[128];
+                Arrays.fill(yPreset, 0, 64, (byte) 1);
+                Arrays.fill(yPreset, 64, 128, (byte) 0);
+                for (int xz = 0; xz < 256; xz++) {
+                    bos.write(yPreset);
+                }
+                bos.write(new byte[16384]);
+                byte[] lightPreset = new byte[16384];
+                Arrays.fill(lightPreset, (byte) 0xFF);
+                bos.write(lightPreset);
+                bos.write(lightPreset);
+                for (int i = 0; i < 256; i++) {
+                    bos.write((byte) 0xFF);
+                }
+                //Biome Colors
+                for (int i = 0; i < 256; i++) {
+                    bos.write((byte) 0x01);
+                    bos.write((byte) 0x85);
+                    bos.write((byte) 0xB2);
+                    bos.write((byte) 0x4A);
+                }
+                bos.write(new byte[4]);
+                FullChunkPacket pkChunk = new FullChunkPacket();
+                pkChunk.chunkX = 1500;
+                pkChunk.chunkZ = 1250;
+                pkChunk.chunkData = bos.toByteArray();
+                cli.sendPacket(pkChunk, true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        cli.sendChat("Moving location... ");
+        MovePlayerPacket pkMove = new MovePlayerPacket(0, 40000.0f, 64.0f, 20000.0f, 0.0f, 0.0f, 0.0f, false);
+        pkMove.mode = MovePlayerPacket.MODE_RESET;
+        cli.sendPacket(pkMove, true);
     }
 
 }
