fd116794d46210539ca4e63ae18538f64a6e8773
TheDiamondYT1 <goldowl37@gmail.com>
2016-06-30 17:44:07 +0100

    testing

--- src/main/java/org/dragonet/proxy/utilities/Terminal.java
+++ src/main/java/org/dragonet/proxy/utilities/Terminal.java
@@ -6,12 +6,12 @@ public class Terminal {
 	public static final String ITALIC		= "\u001B[3m";
 	public static final String UNDERLINE		= "\u001B[4m";
 	
-	public static final String BLACK		= "\u001B[30m";
-	public static final String RED			= "\u001B[31m";
-	public static final String GREEN		= "\u001B[32m";
-	public static final String YELLOW		= "\u001B[33m";
-	public static final String BLUE			= "\u001B[34m";
-	public static final String MAGENTA		= "\u001B[35m";
-	public static final String CYAN			= "\u001B[36m";
-	public static final String WHITE		= "\u001B[37m";
+	public static final String BLACK		= "\u001B[0;30m";
+	public static final String RED			= "\u001B[0;31m";
+	public static final String GREEN		= "\u001B[0;32m";
+	public static final String YELLOW		= "\u001B[0;33m";
+	public static final String BLUE			= "\u001B[0;34m";
+	public static final String MAGENTA		= "\u001B[0;35m";
+	public static final String CYAN			= "\u001B[0;36m";
+	public static final String WHITE		= "\u001B[0;37m";
 }
